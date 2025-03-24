5023172b3c72e1fa2419d9368cb88fcf21604141
naooung <1030n@naver.com>
2025-03-25 08:58:24 +0900

    week2

--- src/week2/문자열내마음대로정렬하기.java
+++ src/week2/문자열내마음대로정렬하기.java
@@ -1,4 +1,13 @@
 package week2;
 
+import java.util.*;
+
 public class 문자열내마음대로정렬하기 {
+    public String[] solution(String[] strings, int n) {
+        // n번째 문자가 같은 문자열 정렬 선 + n번째 문자 비교하여 내림차순 정렬
+        Arrays.sort(strings);
+        Arrays.sort(strings, (o1, o2) -> (o1.charAt(n) - o2.charAt(n)));
+
+        return strings;
+    }
 }
